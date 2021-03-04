06a0f516221bb8df7d84faef3aa420d598000ea9
Mackan <52716811+vpg-code@users.noreply.github.com>
2021-03-04 19:51:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,8 @@ def get_bot_response():
         return (str(":)"))
 
 
-from waitress import serve
+if __name__ == "__main__":
+    from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
 
 
