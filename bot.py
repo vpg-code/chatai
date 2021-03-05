3cf6d1fd56400859b7f88d7cab62cc4ddc72bdae
Mackan <52716811+vpg-code@users.noreply.github.com>
2021-03-05 11:43:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,10 +39,4 @@ def get_bot_response():
 
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
-
-
-
-
-
